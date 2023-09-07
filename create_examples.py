76e0ad3c64bea85539cbb2c9511503fff966cc63
Thomas David <thomasdavid0@gmail.com>
2023-09-07 10:48:56 +0100

    update examples"

--- create_examples.py
+++ create_examples.py
@@ -8,10 +8,11 @@ with open('api/examples/manual_F3A_P23_22_05_31_00000350.json', 'r') as f:
 mans = fcj_to_states(fcj, dict(category="f3a", name="p23"))
 
 for k, m in mans.items():
+    print(m['mdef']['info']['short_name'])
     al = align(fl=m['fl'], mdef=m['mdef'])
 
-    _score = score(al=al, mdef = m['mdef'])
+    _score = score(al=al['al'], mdef = m['mdef'])
 
     with open(f'api/examples/{k}.json', 'w') as f:
-        dump(dict(al=al,busy=False,**_score,), f, cls=NumpyEncoder, ignore_nan=True)
+        dump(dict(**al,busy=False,**_score,), f, cls=NumpyEncoder, ignore_nan=True)
     

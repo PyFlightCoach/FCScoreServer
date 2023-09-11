92acc4b3eefdeb9ddee34c59c758d24d18634961
Thomas David <thomasdavid0@gmail.com>
2023-09-11 07:56:01 +0100

    update examples

--- create_examples.py
+++ create_examples.py
@@ -2,7 +2,7 @@ from simplejson import load, dump
 from app.funcs import fcj_to_states, align, score
 from app.app import NumpyEncoder
 
-with open('api/examples/manual_F3A_P23_22_05_31_00000350.json', 'r') as f:
+with open('app/examples/manual_F3A_P23_22_05_31_00000350.json', 'r') as f:
     fcj = load(f)
 
 mans = fcj_to_states(fcj, dict(category="f3a", name="p23"))
@@ -13,6 +13,6 @@ for k, m in mans.items():
 
     _score = score(al=al['al'], mdef = m['mdef'])
 
-    with open(f'api/examples/{k}.json', 'w') as f:
+    with open(f'app/examples/{k}.json', 'w') as f:
         dump(dict(**al,busy=False,**_score,), f, cls=NumpyEncoder, ignore_nan=True)
     
